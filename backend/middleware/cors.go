5021ed0a433e96ff4ac80fb3d8083c718496d702
rainman456 <81997753+rainman456@users.noreply.github.com>
2025-12-26 18:09:31 +0000

    main

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -5,11 +5,19 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func CORSMiddleware(allowedOrigins []string) echo.MiddlewareFunc {
+func CORSMiddleware(_ []string) echo.MiddlewareFunc {
 	return middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: allowedOrigins,
-		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.OPTIONS},
-		AllowHeaders: []string{echo.HeaderContentType, echo.HeaderAuthorization},
-		MaxAge:       3600, // 1 hour
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{
+			echo.GET,
+			echo.POST,
+			echo.PUT,
+			echo.DELETE,
+			echo.OPTIONS,
+		},
+		AllowHeaders: []string{
+			echo.HeaderContentType,
+			echo.HeaderAuthorization,
+		},
 	})
 }
